387ef262ef338a0d7bf3209fe4fa28a45531abc8
Sandro <sandro.boka@endava.com>
2024-10-22 10:48:10 +0200

    PR fixed to HomeScreenViewModel

--- WeatherApp/WeatherApp/HomeScreen/HomeScreenViewModel.swift
+++ WeatherApp/WeatherApp/HomeScreen/HomeScreenViewModel.swift
@@ -3,7 +3,8 @@ import SwiftUI
 class HomeScreenViewModel: ObservableObject {
 
     private let apiKey = "ff4cd4d2c654b4100a2712f4cbaeb732"
-    @Published var weather: WeatherModel?
+
+    @Published private(set) var weather: WeatherModel?
 
     init() {
             Task {
@@ -12,22 +13,22 @@ class HomeScreenViewModel: ObservableObject {
         }
 
     func fetchWeatherZagreb() async {
-        var urlComponents = URLComponents(string: "https://api.openweathermap.org/data/2.5/weather")!
+        guard let urlComponentsCheck = URLComponents(string: "https://api.openweathermap.org/data/2.5/weather") else { return }
+
+        var urlComponents = urlComponentsCheck
         urlComponents.queryItems = [
             URLQueryItem(name: "q", value: "Zagreb"),
             URLQueryItem(name: "appid", value: apiKey),
-            URLQueryItem(name: "units", value: "metric")
-        ]
+            URLQueryItem(name: "units", value: "metric")]
 
         guard let url = urlComponents.url else { return }
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let decodedData = try JSONDecoder().decode(WeatherModel.self, from: data)
-            DispatchQueue.main.async {
-                self.weather = decodedData
+            DispatchQueue.main.async { [weak self] in
+                self?.weather = decodedData
             }
-            print(decodedData)
         } catch {
             print("Error fetching weather data: \(error)")
         }
