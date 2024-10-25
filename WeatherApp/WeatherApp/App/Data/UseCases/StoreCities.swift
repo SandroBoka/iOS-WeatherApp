0186d5bd7d5b9f25afafc3f35788e6ab56ccd792
Sandro <sandro.boka@endava.com>
2024-10-25 14:53:01 +0200

    StoreCitiesUseCase added

--- WeatherApp/WeatherApp/App/Data/UseCases/StoreCities.swift
+++ WeatherApp/WeatherApp/App/Data/UseCases/StoreCities.swift
@@ -1,8 +1,21 @@
-//
-//  StoreCities.swift
-//  WeatherApp
-//
-//  Created by Sandro Boka on 25.10.2024..
-//
-
 import Foundation
+
+protocol StoreCitiesUseCaseProtocol {
+
+    func storeCities(cities: [City])
+
+}
+
+class StoreCitiesUseCase: StoreCitiesUseCaseProtocol {
+
+    private let dataRepo: DataRepositoryProtocol
+
+    init(dataRepo: DataRepositoryProtocol) {
+        self.dataRepo = dataRepo
+    }
+
+    func storeCities(cities: [City]) {
+        dataRepo.storeCities(cities: cities)
+    }
+
+}
