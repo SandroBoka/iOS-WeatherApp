6048962b4c00f1ac415edbad4603e4f8c52a43b1
Sandro <sandro.boka@endava.com>
2024-10-25 14:50:28 +0200

    GetCitiesUseCase added

--- WeatherApp/WeatherApp/App/Data/UseCases/GetCities.swift
+++ WeatherApp/WeatherApp/App/Data/UseCases/GetCities.swift
@@ -1,8 +1,21 @@
-//
-//  GetCities.swift
-//  WeatherApp
-//
-//  Created by Sandro Boka on 25.10.2024..
-//
-
 import Foundation
+
+protocol GetCitiesUseCaseProtocol {
+
+    func getCities() -> [City]
+
+}
+
+class GetCitiesUseCase: GetCitiesUseCaseProtocol {
+
+    private let dataRepo: DataRepositoryProtocol
+
+    init(dataRepo: DataRepositoryProtocol) {
+        self.dataRepo = dataRepo
+    }
+
+    func getCities() -> [City] {
+        return dataRepo.getCities()
+    }
+    
+}
