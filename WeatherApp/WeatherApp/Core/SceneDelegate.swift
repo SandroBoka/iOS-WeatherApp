aba0889d40508b78d8305367084e9b8603ee7744
Sandro <sandro.boka@endava.com>
2024-10-30 11:00:56 +0100

    PR fixes to SceneDelegate

--- WeatherApp/WeatherApp/Core/SceneDelegate.swift
+++ WeatherApp/WeatherApp/Core/SceneDelegate.swift
@@ -3,7 +3,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    lazy var dependencies = Dependencies()
+    private lazy var dependencies = Dependencies()
 
     func scene(
         _ scene: UIScene,
