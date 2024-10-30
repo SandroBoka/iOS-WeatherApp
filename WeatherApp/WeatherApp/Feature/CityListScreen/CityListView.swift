4b7a3533e8ef203ea097bbeb9cfcdbb0a22c8f3e
Sandro <sandro.boka@endava.com>
2024-10-30 10:58:48 +0100

    PR fixes to CityListView

--- WeatherApp/WeatherApp/Feature/CityListScreen/CityListView.swift
+++ WeatherApp/WeatherApp/Feature/CityListScreen/CityListView.swift
@@ -38,8 +38,5 @@ struct CityListView: View {
         viewModel: CityListViewModel(
             router: Router(navigationController: UINavigationController(), viewModelFactory: Dependencies()),
             useCase: GetWeatherUseCase(
-                weatherRepo: WeatherRepository(weatherService: WeatherService(client: NetworkClient()))
-            )
-        )
-    )
+                weatherRepo: WeatherRepository(weatherService: WeatherService(client: NetworkClient())))))
 }
