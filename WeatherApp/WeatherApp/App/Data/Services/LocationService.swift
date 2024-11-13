f37d8eedf7705c2ede0445fed2395e3c6d953cc3
Sandro <sandro.boka@endava.com>
2024-11-13 11:16:37 +0100

    LocationService now uses InfoConstants

--- WeatherApp/WeatherApp/App/Data/Services/LocationService.swift
+++ WeatherApp/WeatherApp/App/Data/Services/LocationService.swift
@@ -15,7 +15,7 @@ class LocationService: LocationServiceProtocol {
     init(client: BaseApiClientProtocol) {
         self.client = client
 
-        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else { fatalError("API Key not found") }
+        guard let apiKey = InfoConstants.openWeatherMapApiKey else { fatalError("API Key not found") }
 
         endPointFactory = LocationEndpointFactory(apiKey: apiKey)
     }
